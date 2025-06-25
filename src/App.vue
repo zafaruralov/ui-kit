0126bd1c3df75934107c334f52dfbd2943515bb7
zafar <sultonovzafar007@gmail.com>
2025-06-25 18:00:18 +0500

    feature: adding button for test

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="app">
+    <BaseButton color="primary" size="small-x" variant="solid" :disabled="false">
+      <div>Уведомления</div>
+      <img src="/public/icon.svg" alt="icon" />
+    </BaseButton>
+    <BaseButton color="secondary" size="small" variant="outline" :disabled="false">
+      <template #default> Уведомления </template>
+    </BaseButton>
+    <BaseButton color="info" size="medium" variant="tag" :disabled="false">
+      <template #default> Уведомления </template>
+    </BaseButton>
+    <BaseButton color="success" size="large" variant="text" :disabled="false">
+      <template #default> Уведомления </template>
+    </BaseButton>
+    <BaseButton color="warning" size="large-x" variant="icon" :disabled="false">
+      <img src="/public/icon.svg" alt="icon" />
+    </BaseButton>
+    <!-- Badge -->
+    <div class="need-space">
+      <BaseButton badgeText="hover:top" badgePosition="on-hover" badgeDirection="top"> hover top </BaseButton>
+      <BaseButton badgeText="block:right" badgePosition="in-block" badgeDirection="right"> in-block right </BaseButton>
+      <BaseButton badgeText="top:left" badgePosition="on-top" badgeDirection="left"> on-top left </BaseButton>
+    </div>
+
+    <!-- Group -->
+    <div class="button-group">
+      <BaseButton :grouped="true">test1</BaseButton>
+      <BaseButton :grouped="true">test2</BaseButton>
+      <BaseButton :grouped="true">test3</BaseButton>
+    </div>
+  </div>
+</template>
+<script setup>
+import BaseButton from "./components/BaseButton.vue";
+</script>
+<style>
+.need-space {
+  padding: 40px;
+  display: flex;
+  gap: 200px;
+}
+</style>
---
+++ src/components/Icon.vue
@@ -0,0 +1,37 @@
+<template>
+    <svg 
+      :width="size" 
+      :height="size" 
+      viewBox="0 0 24 24" 
+      fill="none" 
+      stroke="currentColor" 
+      stroke-width="2" 
+      stroke-linecap="round" 
+      stroke-linejoin="round"
+      :class="className"
+    >
+      <path v-if="name === 'menu'" d="M3 12h18M3 6h18M3 18h18"/>
+      <path v-else-if="name === 'plus'" d="M12 5v14m-7-7h14"/>
+      <path v-else-if="name === 'x'" d="M18 6 6 18M6 6l12 12"/>
+      <path v-else-if="name === 'align-left'" d="M21 6H3M15 12H3M17 18H3"/>
+      <path v-else-if="name === 'align-center'" d="M21 6H3M17 12H7M19 18H5"/>
+      <path v-else-if="name === 'align-right'" d="M21 6H3M21 12H9M21 18H7"/>
+    </svg>
+  </template>
+  
+  <script setup>
+  defineProps({
+    name: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: [String, Number],
+      default: 24
+    },
+    className: {
+      type: String,
+      default: ''
+    }
+  })
+  </script>
